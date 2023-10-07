95c13ae6b6f07ba6ec3db7bc26a676dbd7419cd6
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-07 13:35:56 +0200

    fix vue and tailwind
    ahora se llevan bien

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
+//import 'tailwindcss/tailwind.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
 
 createApp(App).mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+// tailwind.config.js
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
