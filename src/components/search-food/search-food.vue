8ec45cdc85591b37ac0dda3008b6971d91e95722
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-07 14:23:53 +0200

    Update search-food.vue

--- src/components/search-food/search-food.vue
+++ src/components/search-food/search-food.vue
@@ -40,10 +40,10 @@
         <!-- Texto de búsqueda -->
         <div class="relative flex-grow">
             <input type="search" id="search-dropdown"
-                class="block w-full p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                class="block w-96 p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
                 placeholder="Search Recepies" required>
             <button type="submit"
-                class="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                class="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-gray-700 rounded-r-lg border border-gray-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-30">
                 <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"></path>
