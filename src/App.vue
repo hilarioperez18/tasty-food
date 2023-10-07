7a04fd8a6ef754dd5b9ab2080c298a2e91205869
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-07 12:36:47 +0200

    Update App.vue
    creación un app.vue de prueba

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <AppHeader info="pepe" />
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            mensaje: '¡Hola, mundo!'
+        };
+    }
+};
+</script>
+  
+<style>
+/* Estilos CSS aquí */
+</style>
+  
