700cb5f6576587fae03d68709a1a84f7874feafe
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-10 12:02:41 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './assets/main.css'
-//import 'tailwindcss/tailwind.css'
+//import './assets/main.css'
+import 'tailwindcss/tailwind.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
