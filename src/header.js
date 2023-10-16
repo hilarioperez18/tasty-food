5973b06b51336f63e9d5f07efa2aacdd174da3da
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-10-16 21:14:59 +0200

    Update header.js

--- src/header.js
+++ src/header.js
@@ -3,65 +3,65 @@
 let nav = document.getElementById('nav');
 let menu = document.getElementById('enlaces');
 let abrir = document.getElementById('open');
-let botones = document.getElementsByClassName('btn-header');
+//let botones = document.getElementsByClassName('btn-header');
 let cerrado = true;
 
-function menus(){
+function menus() {
     let Desplazamiento_Actual = window.scrollY;
 
-    if(Desplazamiento_Actual <300){
+    if (Desplazamiento_Actual < 300) {
         nav.classList.remove('nav2');
         nav.className = ('nav1');
-        nav.style.transition= '1s';
+        nav.style.transition = '1s';
         menu.style.top = '80px';
         abrir.style.color = '#fff';
-    }else{
+    } else {
         nav.classList.remove('nav1');
         nav.className = ('nav2');
-        nav.style.transition= '1s';
+        nav.style.transition = '1s';
         menu.style.top = '100px';
         abrir.style.color = '#000';
     }
 }
 
-function apertura(){
-    if(cerrado){
+function apertura() {
+    if (cerrado) {
         menu.style.width = '70vw';
         cerrado = false;
-    }else{
+    } else {
         menu.style.width = '0';
         menu.style.overflow = 'hidden';
         cerrado = true;
     }
 }
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     $('#onload').fadeOut();
     $('body').removeClass('hidden');
     menus();
 });
-window.addEventListener('click', function(e){
+window.addEventListener('click', function (e) {
     console.log(e.target);
-    if(cerrado==false){
+    if (cerrado == false) {
         let span = document.querySelector('span');
-        if(e.target !== span && e.target !== abrir){
+        if (e.target !== span && e.target !== abrir) {
             menu.style.width = '0%';
             menu.style.overflow = 'hidden';
             cerrado = true;
         }
     }
 });
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     console.log(window.scrollY);
     menus();
 });
-window.addEventListener('resize', function(){
-    if(this.screen.width>= 700){
+window.addEventListener('resize', function () {
+    if (this.screen.width >= 700) {
         cerrado = true;
         menu.style.removeProperty('overflow');
         menu.style.removeProperty('width');
     }
 });
-abrir.addEventListener('click', function(){
+abrir.addEventListener('click', function () {
     apertura();
 });
