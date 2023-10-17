9dfb39ef1f084d4e972168e12b2a8d4485da5917
gonzsanz <davidgonzsanz02@gmail.com>
2023-10-17 21:29:30 +0200

    routing funcionando

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
-    <div>
+    <div class="container">
         <HeaderComponent />
         <router-link to="/" />
     </div>
     <RouterView />
 </template>
-  
+
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
-import templateApp from "./components/TemplateApp.vue";
 import { RouterLink, RouterView } from 'vue-router'
 
 export default {
     data() {
         return {
-            mensaje: '¡Hola, mundo!'
         };
     },
     name: "App",
     components: {
         HeaderComponent,
-        templateApp,
         RouterLink,
         RouterView
 
     },
 };
 </script>
+
+<style></style>
