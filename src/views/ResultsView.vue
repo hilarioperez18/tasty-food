5049af292032e4944db71b382a1dbb1646512f7d
ivan <ivanmartinezdelatorre06@gmail.com>
2023-10-20 19:11:06 +0200

    funciona

--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -1,25 +1,33 @@
 <template lang="">
     <div id="test">
-        
+        <searchFood class="ml-9" />
+        <VListSearchRecipe :recipes="result"></VListSearchRecipe>
     </div>
 </template>
 <script>
+import VListSearchRecipe from '../components/VListSearchRecipe.vue';
+import searchFood from '../components/search-food/search-food.vue';
+
 export default {
+    components:{
+        VListSearchRecipe,
+        searchFood
+    },
     data() {
         return {
             result: [],
         }
     },
     beforeMount() {
-        this.result = localStorage.getItem("result");
-        console.log(JSON.parse(this.result));
+        this.result = JSON.parse(localStorage.getItem("result"))
+        console.log(this.result);
     }
 }
 </script>
 <style>
 #test {
     background-color: brown;
-    height: 100px;
-    width: 100px;
+    height: max-content;
+    width: max-content;
 }
 </style>
