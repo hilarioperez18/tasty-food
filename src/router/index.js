d52f3ae1ed5096021a6b439cf9c197cb41928f2f
bflores96 <barbii.f26@gmail.com>
2023-10-22 23:11:57 +0200

    Landing

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import DetailsView from '../views/DetailsView.vue'
 import HomeView from '../views/HomeView.vue'
 import ResultsView from '../views/ResultsView.vue'
+import LandingPage from '../views/LandingPage.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
+            name: 'landing',
+            component: LandingPage
+        },
+        {
+            path: '/home',
             name: 'home',
             component: HomeView
         },
