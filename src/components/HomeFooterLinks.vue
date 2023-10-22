7783ed0500c943cea8d25845badb28fbc2fedf4e
Hilario <hilarioperez18@gmail.com>
2023-10-22 12:35:21 +0200

    Add Footer Component

---
+++ src/components/HomeFooterLinks.vue
@@ -0,0 +1,27 @@
+<template>
+    <a :href="url" target="_blank" :style="{ color: textColor }" @mouseover="changeColor(colorHover)"
+        @mouseout="changeColor(colorNatural)">
+        <slot></slot>
+    </a>
+</template>
+  
+<script>
+export default {
+    props: {
+        url: String,
+        colorNatural: String,
+        colorHover: String,
+    },
+    data() {
+        return {
+            textColor: this.colorNatural,
+        };
+    },
+    methods: {
+        changeColor(color) {
+            this.textColor = color;
+        },
+    },
+};
+</script>
+  
