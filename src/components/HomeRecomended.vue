13e082212d2fcef15c16b2e6d7909b1385173eaa
ivan <ivanmartinezdelatorre06@gmail.com>
2023-10-23 19:02:40 +0200

    change23-10

--- src/components/HomeRecomended.vue
+++ src/components/HomeRecomended.vue
@@ -2,35 +2,36 @@
     <section class="recommended bg-gray-100 p-5 text-center">
         <h2 class="text-2xl font-bold mb-6">Our recommendations</h2>
         <div class="cards flex justify-around flex-wrap">
-            <div class="card p-4 border border-gray-300 mr-24 mb-4">
-                <img src="" alt="Recomendado 1">
-                <p class="p-2">Recommended Text 1</p>
-                <RouterLink to="/recipes"><button class="bg-blue-500 text-white px-4 py-2 mt-2">See more</button>
-                </RouterLink>
-            </div>
-            <div class="card p-4 border border-gray-300 mr-24 mb-4">
-                <img src="" alt="Recomendado 2">
-                <p class="p-2">Recommended Text 2</p>
-                <RouterLink to="/recipes"><button class="bg-blue-500 text-white px-4 py-2 mt-2">See more</button>
-                </RouterLink>
-
-            </div>
-            <div class="card p-4 border border-gray-300 mr-24 mb-4">
-                <img src="" alt="Recomendado 3">
-                <p class="p-2">Recommended Text 3</p>
-                <router-link :to="{ name: 'recipes', params: { id: id } }"><button
-                        class="bg-blue-500 text-white px-4 py-2 mt-2">See more</button>
-                </router-link>
-            </div>
+            <!-- <VListSearchRecipe :recipes="recipes"></VListSearchRecipe> -->
         </div>
     </section>
 </template>
 
 <script>
+
+import VListSearchRecipe from './VListSearchRecipe.vue';
+
 export default {
     data() {
         return {
-            id: 1
+            id: 1,
+            recipes: []
+        }
+    },
+    beforeMount() {
+        this.getRecommend();
+    },
+    methods: {
+        getRecommend() {
+            const apiKey = import.meta.env.VITE_API_KEY;
+            let urlRecipes = `https://api.spoonacular.com/recipes/random&apiKey=${apiKey}`;
+            
+            fetch(urlRecipes)
+                .then((response) => response.json())
+                .then((recipe) => {
+                    this.recipes = recipe;
+                    console.log(recipe)
+                })
         }
     },
 
--- src/components/VListSearchRecipe.vue
+++ src/components/VListSearchRecipe.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="max-w-max mx-5 my-5 bg-gray-100">
-        <h2 class="section-title text-left font-bold text-xl mb-4 ml-9">Search results for:</h2>
-        <input type="search"
-            class="block mx-7 w-96 p-2.5 text-sm text-black bg-gray-300 rounded-2xl border-l-gray-50 border-l-2 border border-gray-300dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-black dark:text-blac"
-            :placeholder=searchQuery disabled />
         <div class="recipe-list shadow-md p-4 flex flex-wrap justify-center">
-            <VCardRecipe v-for=" recipe  in  recipes " :key="recipe.id" :imageUrl="recipe.image" :category="x"
+            <VCardRecipe v-for=" recipe  in  recipes " :key="recipe.id" :id="recipe.id" :imageUrl="recipe.image" :category="x"
                 :name="recipe.title" :preparationTime="x">
             </VCardRecipe>
         </div>
@@ -20,15 +16,10 @@ export default {
     components: {
         VCardRecipe,
     },
-    data() {
-        return {
-            searchQuery: "",
-        };
-    },
+
     beforeMount() {
         console.log("Lista")
         console.log(this.recipes);
-        this.searchQuery = localStorage.getItem("inputSearchRecipe")
     },
     props: {
         recipes: Array
